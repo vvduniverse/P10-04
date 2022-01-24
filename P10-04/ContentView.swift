3e6339dd9a6ef3df5eb34ca235f11b4b1e1c3043
Vahtee Boo <vahtee.boo@gmail.com>
2022-01-24 18:15:48 +0200

    Done

--- P10-04/ContentView.swift
+++ P10-04/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var email = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            
+            Section {
+                Button("Create account") {
+                    print("Creating accoun...")
+                }
+            }
+            .disabled(disableForm)
+//            .disabled(username.isEmpty || email.isEmpty)
+        }
+    }
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
     }
 }
 
